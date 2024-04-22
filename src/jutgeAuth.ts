940ad1bfbb97e6d7d4dbe3d07db989ebbffafbbb
alexstedev <4002tsa@gmail.com>
2024-04-22 10:37:14 +0200

    Moves auth functions to separate file

---
+++ src/jutgeAuth.ts
@@ -0,0 +1,68 @@
+import * as vscode from 'vscode';
+import axios from 'axios';
+
+import { AuthenticationService } from './client/services/AuthenticationService';
+
+import { getExtensionContext } from './extension';
+
+export async function isUserAuthenticated(): Promise<boolean> {
+	const context = getExtensionContext();
+	const token = await context.secrets.get('jutgeToken');
+	const tokenCheck = await AuthenticationService.check(); // No args because token is passed in headers
+	const isTokenValid = tokenCheck.success;
+	return token !== undefined && isTokenValid;
+}
+
+export async function signInToJutge() {
+	const context = getExtensionContext();
+
+	const default_email = await context.secrets.get("email") || ''
+	const email = await vscode.window.showInputBox({
+		placeHolder: "your email",
+		prompt: "Please write your email for Jutge.org.",
+		value: default_email,
+	})
+	if (!email) {
+		return
+	}
+
+	const password = await vscode.window.showInputBox({
+		placeHolder: "your password",
+		prompt: "Please write your password for Jutge.org.",
+		value: "",
+		password: true,
+	})
+	if (!password) {
+		return
+	}
+
+	let credentials;
+	try {
+		credentials = await AuthenticationService.login({ email, password })
+	} catch (error) {
+		vscode.window.showErrorMessage('Jutge.org: Invalid credentials to sign in.')
+		return
+	}
+
+	await context.secrets.store("jutgeToken", credentials.token)
+	await context.secrets.store("email", email)
+
+	axios.defaults.headers.common['Authorization'] = 'Bearer ' + credentials.token
+
+	vscode.window.showInformationMessage('Jutge.org: You have signed in.')
+}
+
+export async function signOutFromJutge() {
+	const context = getExtensionContext();
+	await context.secrets.delete("jutgeToken")
+	await context.secrets.delete("email")
+
+	delete axios.defaults.headers.common['Authorization']
+
+	vscode.window.showInformationMessage('Jutge.org: You have signed out.')
+}
+
+export function registerAuthCommands(context: vscode.ExtensionContext) {
+	context.subscriptions.push(vscode.commands.registerCommand('jutge-vscode.signIn', signInToJutge));
+	context.subscriptions.push(vscode.commands.registerCommand('jutge-vscode.signOut', signOutFromJutge));
+}
