bcd445fd7936ee9fd25afd5446f523a4c88b34ad
alexstedev <4002tsa@gmail.com>
2024-04-25 18:19:20 +0200

    Remove src/client from gitignore

--- src/client/models/TSubmissionPostIn.ts
+++
@@ -1,9 +0,0 @@
-/* generated using openapi-typescript-codegen -- do no edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-export type TSubmissionPostIn = {
-  compiler_id: string;
-  annotation?: string;
-  file: Blob;
-};
--- src/client/models/TTestcase.ts
+++
@@ -1,9 +0,0 @@
-/* generated using openapi-typescript-codegen -- do no edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-export type TTestcase = {
-  name: string;
-  input_b64: string;
-  correct_b64: string;
-};
