3b92037cad03747f2834cf2b2d23107cc3179729
alexstedev <4002tsa@gmail.com>
2024-04-28 20:04:15 +0200

    Removes src/client and will make tests fail

--- src/client/models/TCredentialsOAuth2In.ts
+++
@@ -1,9 +0,0 @@
-/* generated using openapi-typescript-codegen -- do no edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-export type TCredentialsOAuth2In = {
-  username: string;
-  password: string;
-  grant_type?: string;
-};
--- src/client/models/TProblemExtrasOut.ts
+++
@@ -1,9 +0,0 @@
-/* generated using openapi-typescript-codegen -- do no edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-export type TProblemExtrasOut = {
-  compilers_with_ac: Array<string>;
-  proglangs_with_ac: Array<string>;
-  official_solution_checks: Record<string, boolean>;
-};
