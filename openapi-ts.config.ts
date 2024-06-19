8bec9b18702a00f5754e54efa50edcc824c9135c
Pau Fernández <pau.fernandez@upc.edu>
2024-06-19 11:53:13 +0200

    Added "/v1" to the API base url.

--- openapi-ts.config.ts
+++ openapi-ts.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "@hey-api/openapi-ts"
 
 export default defineConfig({
     client: "axios",
-    input: "https://api.jutge.org/openapi.json",
-    base: "https://api.jutge.org",
+    input: "https://api.jutge.org/v1/openapi.json",
+    base: "https://api.jutge.org/v1",
     output: "src/client",
 })
