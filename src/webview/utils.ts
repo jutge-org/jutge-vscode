0547353bf617d34d22275c343c98f762de127b57
Pau Fernández <pau.fernandez@upc.edu>
2025-05-06 22:55:22 +0200

    Better implementation of `makeSpecialCharsVisible`

--- src/webview/utils.ts
+++ src/webview/utils.ts
@@ -4,13 +4,24 @@
  * @returns String with special characters made visible
  */
 export function makeSpecialCharsVisible(str: string): string {
-    if (!str) {
-        return ""
+    let result = ""
+    for (const ch of str) {
+        switch (ch) {
+            case " ":
+                result += `<span class="text-muted">·</span>`
+                break
+            case "\n":
+                result += `<span class="text-muted">↵</span> <br>`
+                break
+            case "\t":
+                result += `<span class="text-muted">  →  </span>`
+                break
+            case "\r":
+                result += `<span class="text-muted">⏎</span>`
+                break
+            default:
+                result += ch
+        }
     }
-
-    return str
-        .replace(/\n/g, "↵\n") // Show newlines with symbol and actual break
-        .replace(/ /g, "·") // Show spaces as middle dots
-        .replace(/\t/g, "→   ") // Show tabs as arrow and spaces
-        .replace(/\r/g, "⏎") // Show carriage returns
+    return result
 }
